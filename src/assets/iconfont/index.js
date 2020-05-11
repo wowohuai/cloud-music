76b5ff1816016b7daf76531ab7804c22a5744e4a
Handsomests <502123065@qq.com>
2020-05-11 16:30:08 +0800

    init app

---
+++ src/assets/iconfont/index.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const IconfontStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'iconfont';  /* project id 1796309 */
+    src: url('//at.alicdn.com/t/font_1796309_qkox8maig5f.eot');
+    src: url('//at.alicdn.com/t/font_1796309_qkox8maig5f.eot?#iefix') format('embedded-opentype'),
+    url('//at.alicdn.com/t/font_1796309_qkox8maig5f.woff2') format('woff2'),
+    url('//at.alicdn.com/t/font_1796309_qkox8maig5f.woff') format('woff'),
+    url('//at.alicdn.com/t/font_1796309_qkox8maig5f.ttf') format('truetype'),
+    url('//at.alicdn.com/t/font_1796309_qkox8maig5f.svg#iconfont') format('svg');
+  }
+
+  .iconfont {
+    font-family: "iconfont" !important;
+    font-size: 16px;
+    font-style: normal;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
+
+
+
